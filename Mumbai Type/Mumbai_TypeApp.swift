e81b68995ad9e77d616e5cc51362f767446a69bf
Ashish Gaikwad <ash.gkwd@gmail.com>
2022-12-21 21:48:34 -0500

    Initial Commit

---
+++ Mumbai Type/Mumbai_TypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mumbai_TypeApp.swift
+//  Mumbai Type
+//
+//  Created by Ashish on 21/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Mumbai_TypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
